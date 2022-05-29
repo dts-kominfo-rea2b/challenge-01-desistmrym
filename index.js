7078d290a59d6d551d9469229457c396bf1ded9b
desistmrym <desisitimaryam@gmail.com>
2022-05-29 14:48:02 +0700

    add challenge data structure

--- index.js
+++ index.js
@@ -1,11 +1,94 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const favFirst = new Set([
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Eggy",
+    "Tempura",
+    "Bento",
+    "Eggy",
+    "Padang",
+    "Tteok",
+    "Sushi",
+    "Sushi"
+])
+  
+
+
+const favSec = new Set([
+    "Tempura",
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Padang",
+    "Katsu",
+    "Geprek",
+    "Pancake",
+    "Eggy"
+])
+
+const firstUser = {
+    name: 'Monica',
+    gender: 'Female',
+    age: "17",
+    email: 'monica@dingdong.com',
+    favColor: ['Yellow', 'Pink', 'White', 'Purple'],
+    isHavePet: 'Yes',
+    edication: [
+        {
+            name: 'SD 01',
+            city: 'Jakarta',
+            graduate: '2016',
+        },
+        {
+            name: 'SMP 02',
+            city: 'Jakarta',
+            graduate: '2019',
+        },
+        {
+            name: 'SMA 03',
+            city: 'Tangerang',
+        }
+    ],
+    favRest: favFirst
+};
+const secondUser = {
+    name: 'Wendy',
+    gender: 'Male',
+    age: "23",
+    email: 'wendy@dingdong.com',
+    favColor: ['Blue', 'Black', 'Grey'],
+    isHavePet: 'No',
+    edication: [
+        {
+            name: 'SD 02',
+            city: 'Jakarta',
+            graduate: '2010',
+        },
+        {
+            name: 'SMP 03',
+            city: 'Bogor',
+            graduate: '2013',
+        },
+        {
+            name: 'SMA 01',
+            city: 'Surabaya',
+            graduate: '2016',
+        },
+        {
+            name: 'Universitas Maju',
+            city: 'Tangerang',
+        }
+    ],
+    favRest: favSec
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser,secondUser)
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
